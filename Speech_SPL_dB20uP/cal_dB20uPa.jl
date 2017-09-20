9fa27bc88a3da9e8acfe01b46d2ca7373d67de3b
lixun <lixun.xia@outlook.com>
2017-09-20 23:45:45 +0800

    factorizing

--- Speech_SPL_dB20uP/cal_dB20uPa.jl
+++ Speech_SPL_dB20uP/cal_dB20uPa.jl
@@ -6,60 +6,31 @@ include("../audio-features.jl")
 
 
 
-struct Source{T <: AbstractFloat, U <: Integer}
-    rate::U
-    path::AbstractString
-    use_file_size::Bool
-    start::T
-    stop::T
-    Source{T,U}(x,y,s,p,q) where {T <: AbstractFloat, U <: Integer} = p >= q ? error("start time must be less than stop time") : new(x,y,s,p,q)
-end
-
-
-
-
-# provide source information for speech extraction from the recordings
-# s = Source{Int64,Float64}(target_fs, "PreparationRCV_100_8000_BPF.wav", 0.35, 10.425)
-function extract_clip_and_merge(recording::AbstractString, repeats::U, ρ::Source{T,U}) where {T <: AbstractFloat, U <: Integer}
+function extract_symbol_and_merge(x::Array{T,1}, s::Array{T,1}, rep::U) where {T <: AbstractFloat, U <: Integer}
     
-    # data preparation
-    r, fs = wavread(recording)
-    assert(U(fs) == ρ.rate)
-    r = r[:,1]
-    n = length(r) 
-
-    s, fs =  wavread(ρ.path)
-    assert(U(fs) == ρ.rate)
-    s = mean(s,2)[:,1]
-
-    # if there is any boundary definitions
-    if ρ.use_file_size == false
-        assert(size(s,1) >= U(floor(ρ.rate * ρ.stop)))
-        t0 = 1 + U(floor(ρ.rate * ρ.start)) 
-        t1 = U(floor(ρ.rate * ρ.stop))
-        s = s[t0:t1]        
-    end
+    n = length(x) 
     m = length(s)
-
     
-    ℝ = xcorr(s, r)
+    ℝ = xcorr(s, x)                                
     #display(plot(ℝ))
     𝓡 = sort(ℝ[local_maxima(ℝ)], rev = true)
-    
-    y = zeros(typeof(r[1]), repeats * m)
-    peaks = zeros(Int64, repeats)
+    isempty(𝓡) && ( error("no local maxima found!") )
+
+    y = zeros(T, rep * m)
+    peaks = zeros(Int64, rep)
 
     # find the anchor point
-    ploc = find(x->x==𝓡[1],ℝ)[1]
+    ploc = find(z->z==𝓡[1],ℝ)[1]
     peaks[1] = ploc
     info("peak anchor-[1] in correlation: $ploc")
     lb = n - ploc + 1
     rb = lb + m - 1
-    y[1:m] = r[lb:rb]
+    y[1:m] = x[lb:rb]
     ip = 1
-    if repeats > 1
+
+    if rep > 1
         for i = 2:length(𝓡)
-            ploc = find(x->x==𝓡[i],ℝ)[1]
+            ploc = find(z->z==𝓡[i],ℝ)[1]
             if sum(abs.(peaks[1:ip] - ploc) .> m) == ip
                 ip += 1
                 peaks[ip] = ploc
@@ -67,95 +38,98 @@ function extract_clip_and_merge(recording::AbstractString, repeats::U, ρ::Sourc
                 lb = n - ploc + 1
                 rb = lb + m - 1
                 #display(plot(r[lb:rb]))
-                y[1+(ip-1)*m : ip*m] = r[lb:rb]
-                if ip == repeats
+                y[1+(ip-1)*m : ip*m] = x[lb:rb]
+                if ip == rep
                     break
                 end
             end
         end
         peaks = sort(peaks)
-        info("diff of peak locations: $(diff(peaks)./ρ.rate) seconds")
+        #info("diff of peak locations: $(diff(peaks)./ρ.rate) seconds")
     end
     #display(plot(y))
-    y
+    (y, diff(peaks))
 end
 
 
-
-# p = Frame1D{Int64}(target_fs, block, div(block,4), 0)
-# s = Source{Int64,Float64}(target_fs, "PreparationRCV_100_8000_BPF.wav", 0.35, 10.425)
-function cal_dB20uPa( calib_recording::AbstractString, p::Frame1D{U}, recording::AbstractString, repeats::U, ρ::Source{T,U}; fl = 100, fh = 12000, piston_dbspl = 114.0) where {T <: AbstractFloat, U <: Integer}
+  
+function cal_dB20uPa( 
+    
+    piston_recording::AbstractString,
+    unknown_recording::AbstractString,
+    symbol::AbstractString,
+    repeat::U, 
+    symbol_l,
+    symbol_h,
+    p::Frame1D{U}; 
+    
+    fl = 100, 
+    fh = 12000, 
+    piston_dbspl = 114.0
+    
+    ) where {U <: Integer}
 
     # extracting calibration recording and measurement recording
-    r, fs = wavread(calib_recording)
+    r, fs = wavread(piston_recording)
     assert(U(fs) == p.rate)
-    assert(p.rate == ρ.rate)
+
     rp = power_spectrum(r[:,1], p, p.block, window=hann)
     rp = mean(rp, 2)
 
-    x = extract_clip_and_merge(recording, repeats, ρ)
-    xp = power_spectrum(x, p, p.block, window=hann)
-    xp = mean(xp, 2)
-    
-    fl = U(floor(fl/p.rate * p.block))
-    fh = U(floor(fh/p.rate * p.block))
-    
-    offset = 10*log10(sum_kbn(rp[fl:fh]))
-    return 10*log10(sum_kbn(xp[fl:fh])) + (piston_dbspl - offset)
-end
+    x, fs = wavread(unknown_recording)
+    assert(U(fs) == p.rate)
+    channels = size(x,2)
+    info("file channels $channels")
+    dbspl = zeros(typeof(x[1,1]), channels)
 
-function cal_dB20uPa( calib_recording::AbstractString, p::Frame1D{U}, recording::AbstractString; tl = 5, tr = 15, fl = 100, fh = 12000, piston_dbspl = 114.0) where {U <: Integer}
-    
-        # extracting calibration recording and measurement recording
-        r, fs = wavread(calib_recording)
-        assert(U(fs) == p.rate)
-        rp = power_spectrum(r[:,1], p, p.block, window=hann)
-        rp = mean(rp, 2)
-    
-        x, fs = wavread(recording)
-        assert(U(fs) == p.rate)
-        channels = size(x,2)
-        dBSPL = zeros(typeof(x[1,1]), channels)
+    s, fs =  wavread(symbol)
+    assert(U(fs) == p.rate)
+    s = mean(s,2)[:,1]
 
-        start = 1+U(floor(tl * p.rate))
-        stop = U(floor(tr * p.rate))
-
-        for c = 1:channels
-            xp = power_spectrum(x[start:stop,c], p, p.block, window=hann)
-            xp = mean(xp, 2)
-            fl = U(floor(fl/p.rate * p.block))
-            fh = U(floor(fh/p.rate * p.block))
-            offset = 10*log10(sum_kbn(rp[fl:fh]))
-            dBSPL[c] = 10*log10(sum_kbn(xp[fl:fh])) + (piston_dbspl - offset)    
-        end
-        dBSPL 
+    # to use whole symbol, set symbol_l >= symbol_h
+    if symbol_l < symbol_h
+        assert(size(s,1) >= U(floor(p.rate * symbol_h))) 
+        s = s[1 + U(floor(p.rate * symbol_l)) : U(floor(p.rate * symbol_h))]        
     end
 
-
-
-
-
-
-
-function dBSPL_46AN(recording::AbstractString, symbol::AbstractString, repeats; use_symbol_size=true, start=0.25, stop=10.45)
-    
-    p = Frame1D{Int64}(48000, 16384, div(16384,4), 0)
-    ρ = Source{Float64, Int64}(48000, symbol, use_symbol_size, start, stop)
-    dBSPL = cal_dB20uPa("1000hz-piston-114dBSPL-46AN.wav", p, recording, repeats, ρ)
-    println("SPL = $dBSPL dB")       
+    for c = 1:channels
+        xp, interval = extract_symbol_and_merge(x[:,c], s, repeat)
+        info("diff of peak locations: $(interval./p.rate) seconds")
+        xp = power_spectrum(xp, p, p.block, window=hann)
+        xp = mean(xp, 2)
+        
+        fl = U(floor(fl/p.rate * p.block))
+        fh = U(floor(fh/p.rate * p.block))
+        
+        offset = 10*log10(sum_kbn(rp[fl:fh]))
+        dbspl[c] = 10*log10(sum_kbn(xp[fl:fh])) + (piston_dbspl - offset)
+        info("channel $c: SPL = $(dbspl[c]) dB")           
+    end
+    dbspl
 end
 
-function dBSPL_40AN(recording::AbstractString, symbol::AbstractString, repeats; use_symbol_size=true, start=0.25, stop=10.45)
-    
-    p = Frame1D{Int64}(48000, 16384, div(16384,4), 0)
-    ρ = Source{Float64, Int64}(48000, symbol, use_symbol_size, start, stop)
-    dBSPL = cal_dB20uPa("1000hz-piston-114dBSPL-40AN.wav", p, recording, repeats, ρ)
-    println("SPL = $dBSPL dB")       
-end
 
-function dBSPL_46AN(recording::AbstractString; start=5, stop=15)
+
+
+function dBSPL_46AN( recording, symbol; 
+    repeat = 3,
+    symbol_start=0.25,
+    symbol_stop=10.45,
+    fl = 100, 
+    fh = 12000, 
+    piston_dbspl = 114.0
+    )
     
     p = Frame1D{Int64}(48000, 16384, div(16384,4), 0)
-    dBSPL = cal_dB20uPa("1000hz-piston-114dBSPL-46AN.wav", p, recording, tl = start, tr = stop)
-    println("SPL = $dBSPL dB")       
+    dBSPL = cal_dB20uPa(
+        "1000hz-piston-114dBSPL-46AN.wav", 
+        recording, 
+        symbol, 
+        repeat, 
+        symbol_start, 
+        symbol_stop, 
+        p,
+        fl = fl,
+        fh = fh,
+        piston_dbspl = piston_dbspl)    
 end
